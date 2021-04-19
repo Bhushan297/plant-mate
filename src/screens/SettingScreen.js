db9685599d8e551de7e56f4a86d7b70d5621f7f2
Shubham Suryavanshi <shubhamsuryavanshi404@gmail.com>
2021-04-19 07:53:18 +0000

    Update src/screens/SettingScreen.js

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Layout, Button, Text } from '@ui-kitten/components';
+import * as SecureStore from 'expo-secure-store';
 
-const SettingScreen = () => {
+const SettingScreen = ({parent}) => {
+
+    const logoutCall = async () =>{
+        await SecureStore.setItemAsync("loggedIn", "false");
+        parent.replace('Login');
+    }
+
     return(
         <Layout level='2' style={styles.container}>
             <Text category='s1'>Plant Mitra v1.0</Text>
-            <Button style={styles.logoutBtn} status='danger'>Log Out</Button>
+            <Button style={styles.logoutBtn} status='danger' onPress={() => logoutCall()}>Log Out</Button>
         </Layout>
     )
 }
