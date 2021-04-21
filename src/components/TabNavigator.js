345ab94a30c859e21b4d636266ab63907496c7e0
shubhamsurya <shubhamsuryavanshi404@gmail.com>
2021-04-21 17:44:22 +0530

    integrated predict

--- src/components/TabNavigator.js
+++ src/components/TabNavigator.js
@@ -54,7 +54,7 @@ export default TabNavigation = ({ navigation }) => {
 					name="Home"
 					children={() => <HomeScreen parent={navigation} />}
 				/>
-				<Tab.Screen name="Upload" component={PicUploadScreen} />
+				<Tab.Screen name="Upload" children={() => <PicUploadScreen parent={navigation} />} />
 				<Tab.Screen name="FAQ" component={FaqScreen} />
 				<Tab.Screen name="Settings" children={() => <SettingsScreen parent={navigation} />} />
 			</Tab.Navigator>
